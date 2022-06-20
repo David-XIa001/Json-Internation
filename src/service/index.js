2ea35941d92bf48f698ce5f31f4a79d2e5c8899f
xiaziwei <xiaziwei@megvii.com>
2022-06-20 16:59:03 +0800

    feat:使用Jsonp

--- src/service/index.js
+++ src/service/index.js
@@ -1,9 +1,30 @@
-import AjaxRequest from "@/utility/AjaxRequest";
+// import AjaxRequest from "@/utility/AjaxRequest";
+// const Qs = require("qs");
+// export function transtoEn(params) {
+//   return AjaxRequest({
+//     url: "/api/trans/vip/translate?" + Qs.stringify(params),
+//     method: "get",
+//     headers: { "Content-type": "application/x-www-form-urlencoded" },
+//   });
+// }
+import jsonp from "jsonp";
 const Qs = require("qs");
+const ajaxUrl = "http://api.fanyi.baidu.com/api/trans/vip/translate?";
 export function transtoEn(params) {
-  return AjaxRequest({
-    url: "/api/trans/vip/translate?" + Qs.stringify(params),
-    method: "get",
-    headers: { "Content-type": "application/x-www-form-urlencoded" },
+  return new Promise((resolve, reject) => {
+    jsonp(ajaxUrl + Qs.stringify(params), null, (err, data) => {
+      if (!err) {
+        resolve(data);
+      } else {
+        reject(err);
+      }
+    });
   });
 }
+
+//  {
+//   "common": {
+//       "laSave": "保存",
+//       "standby": "待机界面自定义"
+//   }
+// }
